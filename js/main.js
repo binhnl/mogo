a670efe85d3db90215e233820375f78dfa1a1b3d
binhnl <binhnl@vietis.com.vn>
2024-05-24 13:55:53 +0700

    update: responsive and animation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 $(document).ready(function () {
-    $('.service-design-accordion-smooth-header').on('click', function () {
-        let $clickedItem = $(this);
-        $clickedItem.siblings('.service-design-accordion-smooth__panel').each(function () {
-            $('.service-design-accordion-smooth__panel').css('display', 'none');
-            $(this).toggle();
-        });
+    AOS.init();
+    $('#header-mobile--icon').on('click', function () {
+        let navMobile = $('.header-menu-list');
+        navMobile.toggleClass('menu-mobile--show');
+    });
+
+    $('.service-design-accordion-smooth-header').click(function(){
+        $(this).toggleClass('active');
+        $(this).next().slideToggle('fast');
+        $('.service-design-accordion-smooth__panel').not($(this).next()).slideUp('fast');
+        $('.service-design-accordion-smooth-header').not($(this)).removeClass('active');
     });
 
     $('.quote-list').slick({
@@ -17,5 +22,29 @@ $(document).ready(function () {
         fadeSpeed: 1000,
         prevArrow: '<span class="slick-prev"><i class="fa-solid fa-chevron-right"></i></span>',
         nextArrow: '<span class="slick-next"><i class="fa-solid fa-chevron-left"></i></span>'
+    });
+
+
+    function countUp($element, target, duration) {
+        let start = 0;
+        let end = target;
+        let increment = end > start ? 1 : -1;
+        let stepTime = Math.abs(Math.floor(duration / (end - start)));
+
+        let timer = setInterval(function() {
+            start += increment;
+            $element.text(start);
+            if (start === end) {
+                clearInterval(timer);
+            }
+        }, stepTime);
+    }
+
+    $('.facts-list-item__number').each(function() {
+        let $this = $(this);
+        let target = parseInt($this.attr('data-target'));
+        let duration = 5000;
+
+        countUp($this, target, duration);
     });
 });
