f7f99cb536b1bf5f4fe90f2945a265fc69df2a32
binhnl <binhnl@vietis.com.vn>
2024-05-27 08:16:29 +0700

    update: update code by review

--- js/main.js
+++ js/main.js
@@ -1,50 +1,104 @@
 $(document).ready(function () {
-    AOS.init();
-    $('#header__icon').on('click', function () {
-        let navMobile = $('.header-menu-list');
-        navMobile.toggleClass('menu-mobile--show');
-    });
+  AOS.init();
 
-    $('.service-design-accordion-smooth-header').click(function(){
-        $(this).toggleClass('active');
-        $(this).next().slideToggle('fast');
-        $('.service-design-accordion-smooth__panel').not($(this).next()).slideUp('fast');
-        $('.service-design-accordion-smooth-header').not($(this)).removeClass('active');
-    });
+  $(".banner-list").slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+    fadeSpeed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: ".banner-nav-list",
+  });
 
-    $('.quote-list').slick({
-        infinite: true,
-        dots: false,
-        arrows: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        fade: true,
-        fadeSpeed: 1000,
-        prevArrow: '<span class="slick-prev"><i class="fa-solid fa-chevron-right"></i></span>',
-        nextArrow: '<span class="slick-next"><i class="fa-solid fa-chevron-left"></i></span>'
-    });
+  $(".banner-nav-list").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: ".banner-list",
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+  });
 
+  $(".quote-list").slick({
+    infinite: true,
+    dots: false,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+    fadeSpeed: 1000,
+    prevArrow:
+      '<span class="slick-prev"><i class="fa-solid fa-chevron-right"></i></span>',
+    nextArrow:
+      '<span class="slick-next"><i class="fa-solid fa-chevron-left"></i></span>',
+  });
 
-    function countUp($element, target, duration) {
-        let start = 0;
-        let end = target;
-        let increment = end > start ? 1 : -1;
-        let stepTime = Math.abs(Math.floor(duration / (end - start)));
-
-        let timer = setInterval(function() {
-            start += increment;
-            $element.text(start);
-            if (start === end) {
-                clearInterval(timer);
-            }
-        }, stepTime);
+  window.addEventListener("scroll", function () {
+    let header = document.getElementById("header");
+    if (window.pageYOffset > 0 && window.innerWidth > 767) {
+      header.classList.add("header--permanent");
+    } else {
+      header.classList.remove("header--permanent");
     }
+  });
 
-    $('.facts-list-item__number').each(function() {
-        let $this = $(this);
-        let target = parseInt($this.attr('data-target'));
-        let duration = 5000;
+  let toogleNavMobile = document.getElementById("header__icon");
+  toogleNavMobile.addEventListener("click", function () {
+    let navMobile = document.querySelector(".header-menu-list");
+    navMobile.classList.toggle("menu-mobile--show");
+  });
 
-        countUp($this, target, duration);
+  let accordionHeader = document.querySelectorAll(
+    ".service-design-accordion-smooth__header"
+  );
+  let accordionPanel = document.querySelectorAll(
+    ".service-design-accordion-smooth__panel"
+  );
+
+  accordionHeader.forEach((header) => {
+    header.addEventListener("click", function () {
+      this.classList.toggle("active");
+      const panel = this.nextElementSibling;
+      panel.style.display = panel.style.display === "block" ? "none" : "block";
+
+      accordionPanel.forEach((otherPanel) => {
+        if (otherPanel !== panel) {
+          otherPanel.style.display = "none";
+        }
+      });
+
+      accordionHeader.forEach((otherHeader) => {
+        if (otherHeader !== this) {
+          otherHeader.classList.remove("active");
+        }
+      });
     });
+  });
+
+  let factsNumber = document.querySelectorAll(".facts-list-item__number");
+  factsNumber.forEach(function (item) {
+    let target = parseInt(item.getAttribute("data-target"));
+    let duration = 5000;
+
+    countUp(item, target, duration);
+  });
+
+  function countUp(element, target, duration) {
+    let start = 0;
+    let end = target;
+    let increment = end > start ? 1 : -1;
+    let stepTime = Math.abs(Math.floor(duration / (end - start)));
+
+    let timer = setInterval(function () {
+      start += increment;
+      element.textContent = start;
+      if (start === end) {
+        clearInterval(timer);
+      }
+    }, stepTime);
+  }
 });
